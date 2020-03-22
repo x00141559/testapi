08605239196a632ede84430cf066bc3430efc455
Nicolae Anastase <32193210+x00143663@users.noreply.github.com>
2020-03-22 20:57:54 +0000

    Add files via upload

---
+++ Stock/Controllers/StockListing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Models
+{
+    public class StockListing
+    {
+        // ticker symbol e.g. AAPL, GOOG, IBM, MSFT
+        [Required]
+        public string TickerSymbol
+        {
+            get;
+            set;
+        }
+
+        // price last trade in $
+       
+        public string Msg
+        {
+            get;
+            set;
+        }
+
+        // todo: add low price, high price, volume etc.
+
+    }
+}
+
